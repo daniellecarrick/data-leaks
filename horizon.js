89480b45330c8b019e8db50ddcab3c9f07e929e4
Danielle Carrick <dacarrick@gmail.com>
2017-11-17 18:29:10 -0500

    ugly 5 columns and new tooltip style

--- horizon.js
+++ horizon.js
@@ -14,8 +14,8 @@
 
          var color = d3.scale.linear()
              .domain([-1, 0, 0, 1])
-            // .range(['#DAE8F3', '#1F77B4']);
-         .range(['#000000', '#DAE8F3', '#DAE8F3', '#1F77B4']);
+             // .range(['#DAE8F3', '#1F77B4']);
+             .range(['#000000', '#DAE8F3', '#DAE8F3', '#1F77B4']);
 
 
          var margin = { top: 0, right: 40, bottom: 20, left: 20 };
@@ -142,70 +142,71 @@
                   Titles generated here
                  ********************/
 
-                var parentClass = this.parentNode.id;
+                 var parentClass = this.parentNode.id;
 
-                // THESE THINGS ONLY HAPPEN TO THE CHARTS NOT TO THE LEGEND
-                if (parentClass === 'chart-container') {
+                 // THESE THINGS ONLY HAPPEN TO THE CHARTS NOT TO THE LEGEND
+                 if (parentClass === 'chart-container') {
 
 
-                 var titles_arr = ['Hacking, Skimming, and Phishing', 'Insider Theft', 'Weak Corporate Internet Security', 'Data Breaches from Lost/Stolen Devices', 'Leak by Outside Vendor'];
-                 var title_bg_width_arr = [160, 70, 160, 190, 120];
-                 var title_bg_width = title_bg_width_arr[counter];
-                 var the_title = titles_arr[counter];
-                 var titles = g.append('g').attr('transform', 'translate(6, 65)').attr('class', 'title-container');
-                 titles.append('rect').attr('class', 'title-bg').attr('width', title_bg_width); // go here for more info: https://github.com/d3/d3/issues/252
-                 titles.append('text').attr('class', 'titles').text(the_title).attr('transform', 'translate(5,12)');
+                     var titles_arr = ['Hacking, Skimming, and Phishing', 'Insider Theft', 'Weak Corporate Internet Security', 'Data Breaches from Lost/Stolen Devices', 'Leak by Outside Vendor'];
+                     var title_bg_width_arr = [160, 70, 160, 190, 120];
+                     var title_bg_width = title_bg_width_arr[counter];
+                     var the_title = titles_arr[counter];
+                     var titles = g.append('g').attr('transform', 'translate(6, 65)').attr('class', 'title-container');
+                     titles.append('rect').attr('class', 'title-bg').attr('width', title_bg_width); // go here for more info: https://github.com/d3/d3/issues/252
+                     titles.append('text').attr('class', 'titles').text(the_title).attr('transform', 'translate(5,12)');
 
-                 /*******************
-                   Annotations generated here
-                 ********************/
+                     /*******************
+                       Annotations generated here
+                     ********************/
 
-                 var annotations_arr = [{
-                         "title": "Hacking, Skimming, Phishing",
-                         "text": "<a href='https://www.wired.com/story/netflix-phishing-scam/?mbid=graphic_cybersecurity' target='_parent'>These attacks doubled <br>from 2015 to 2016</a>",
-                         "coordinates": [2014, 40]
-                     },
-                     {
-                         "title": "Insider Theft",
-                         "text": "<a href='https://www.wired.com/story/hbo-hacks-game-of-thrones/?mbid=graphic_cybersecurity'  target='_parent'>Sys Admins: Remember to revoke privileges from former employees.</a>",
-                         "coordinates": [2008, 40]
-                     },
-                     {
-                         "title": "Weak Corporate Internet Security",
-                         "text": "<a href='https://www.wired.com/2009/07/health-breaches/?mbid=graphic_cybersecurity'  target='_parent'>Firms inadvertently released personal data online 109 times in 2015.</a>",
-                         "coordinates": [2012, 30]
-                     },
-                     {
-                         "title": "Lost Stolen Devices",
-                         "text": "<a href='https://www.wired.com/2010/04/iphone-finder/?mbid=graphic_cybersecurity'  target='_parent'>Better security protocols mean that stolen or lost devices stay locked.</a>",
-                         "coordinates": [2013, 30]
-                     }, {
-                         "title": "Leak by Outside Vendor",
-                         "text": "<a href='https://www.wired.com/2017/03/protect-icloud-account-juuuuust-case/?mbid=graphic_cybersecurity'  target='_parent'>Any type of breach could expose the data of thousands (or millions) of people.</a>",
-                         "coordinates": [2012, 30]
+                     var annotations_arr = [{
+                             "title": "Hacking, Skimming, Phishing",
+                             "text": "<a href='https://www.wired.com/story/netflix-phishing-scam/?mbid=graphic_cybersecurity' target='_parent'>These attacks doubled <br>from 2015 to 2016</a>",
+                             "coordinates": [2014, 40]
+                         },
+                         {
+                             "title": "Insider Theft",
+                             "text": "<a href='https://www.wired.com/story/hbo-hacks-game-of-thrones/?mbid=graphic_cybersecurity'  target='_parent'>Sys Admins: Remember to revoke privileges from former employees.</a>",
+                             "coordinates": [2008, 40]
+                         },
+                         {
+                             "title": "Weak Corporate Internet Security",
+                             "text": "<a href='https://www.wired.com/2009/07/health-breaches/?mbid=graphic_cybersecurity'  target='_parent'>Firms inadvertently released personal data online 109 times in 2015.</a>",
+                             "coordinates": [2012, 30]
+                         },
+                         {
+                             "title": "Lost Stolen Devices",
+                             "text": "<a href='https://www.wired.com/2010/04/iphone-finder/?mbid=graphic_cybersecurity'  target='_parent'>Better security protocols mean that stolen or lost devices stay locked.</a>",
+                             "coordinates": [2013, 30]
+                         }, {
+                             "title": "Leak by Outside Vendor",
+                             "text": "<a href='https://www.wired.com/2017/03/protect-icloud-account-juuuuust-case/?mbid=graphic_cybersecurity'  target='_parent'>Any type of breach could expose the data of thousands (or millions) of people.</a>",
+                             "coordinates": [2012, 30]
+                         }
+                     ];
+
+                     // var the_annotation = annotations_arr[counter].text;
+                     var translate_x = x1(annotations_arr[counter].coordinates[0]);
+                     var translate_y = 10;
+                     var annotations = g.append('circle').attr('transform', 'translate(' + (translate_x) + ',' + translate_y + ')');
+
+
+                     // since this gets redone on resize, need to reset counter
+                     counter++;
+                     if (counter === 5) {
+                         counter = 0;
                      }
-                 ];
 
-                // var the_annotation = annotations_arr[counter].text;
-                 var translate_x = x1(annotations_arr[counter].coordinates[0]);
-                 var translate_y = 10;
-                 var annotations = g.append('circle').attr('transform', 'translate(' + (translate_x) + ',' + translate_y + ')');
-
-
-                 // since this gets redone on resize, need to reset counter
-                 counter++;
-                 if (counter === 5) {
-                     counter = 0;
                  }
-
-                }
 
                  /*******************
                    Tooltip generated here
                  ********************/
                  var tooltip = g.append('g').attr('class', 'tooltip-container');
-                 tooltip.append('line').attr('stroke', 'black').attr('class', 'tooltip-line');
-                 tooltip.append('text').attr('class', 'tooltip-text').attr('fill', 'white');
+                 tooltip.append('line').attr('stroke', 'black').attr('class', 'tooltip-line-top');
+                 tooltip.append('line').attr('stroke', 'black').attr('class', 'tooltip-line-bottom');
+                 tooltip.append('text').attr('class', 'tooltip-text').attr('fill', 'black');
 
                  var date_arr = [2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017];
 
@@ -216,9 +217,11 @@
                      var mouse_x = d3.mouse(this)[0],
                          x0 = x1.invert(d3.mouse(this)[0]);
                      // Add a vertical line
-                     d3.selectAll('.tooltip-line').attr('x1', mouse_x).attr('x2', mouse_x).attr('y1', 0).attr('y2', 100);
+                     d3.selectAll('.tooltip-line-top').attr('x1', mouse_x).attr('x2', mouse_x).attr('y1', 0).attr('y2', 40);
+                      d3.selectAll('.tooltip-line-bottom').attr('x1', mouse_x).attr('x2', mouse_x).attr('y1', 55).attr('y2', 100);
+
                      // Add the data label
-                     d3.selectAll('svg .tooltip-text').attr('x', mouse_x + 10).attr('y', 60)
+                     d3.selectAll('svg .tooltip-text').attr('x', mouse_x - 35).attr('y', 50)
                          .text(function(d, i) {
                              var z = bisect(date_arr, x0, 1);
                              var y_val = d[z - 1][1];
@@ -246,20 +249,20 @@
              return horizon;
          };
 
-       horizon.bands = function(x) {
-      if (!arguments.length) return bands;
-      bands = +x;
-      color.domain([ -bands, 0, 0, bands]);
-      return horizon;
-    };
-
-         // when there are no negatives, this function works better with the colors
-       /*  horizon.bands = function(x) {
+         horizon.bands = function(x) {
              if (!arguments.length) return bands;
              bands = +x;
-             color.domain([0, bands]);
+             color.domain([-bands, 0, 0, bands]);
              return horizon;
-         };*/
+         };
+
+         // when there are no negatives, this function works better with the colors
+         /*  horizon.bands = function(x) {
+               if (!arguments.length) return bands;
+               bands = +x;
+               color.domain([0, bands]);
+               return horizon;
+           };*/
 
          horizon.mode = function(x) {
              if (!arguments.length) return mode;
